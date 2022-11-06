f1f6293585b7035827eee0db92c16615492cdefe
Shawal Ali <shawalnetwork@gmail.com>
2022-11-06 17:29:14 +0500

    snippet details added

--- src/components/snippets/SnippetDetails.jsx
+++ src/components/snippets/SnippetDetails.jsx
@@ -1,7 +1,91 @@
 import './snippetDetails.css';
 
 const SnippetDetails = () => {
-  return <div className='snippetDetails'>SnippetDetails</div>;
+  return (
+    <div className='snippetDetails'>
+      <div className='snipDetContainer'>
+        <div className='snipDetTop'>
+          <span>Snippet</span>
+          <h1>useMouseHook</h1>
+        </div>
+        <div className='snipDetCenter'>
+          <span className='snipTag'>JS</span>
+          <div className='snipDetCode'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+              quo, aliquam praesentium voluptatibus at placeat temporibus
+              aperiam ipsum, sequi libero repellendus nisi velit voluptate
+              mollitia? Repellendus omnis culpa consequuntur deleniti asperiores
+              numquam facilis veritatis nemo dolorem nulla eum cupiditate
+              obcaecati perspiciatis eveniet eius, nobis vel sapiente esse
+              distinctio doloribus! Perspiciatis molestias ex blanditiis id
+              nesciunt provident nihil exercitationem similique quos. Sit
+              expedita perferendis, placeat perspiciatis porro reiciendis rerum
+              iusto, eius eligendi laboriosam accusantium. Quasi magnam animi
+              possimus suscipit voluptate neque, delectus nesciunt et odio porro
+              quis, laborum eligendi vitae tempore nemo reprehenderit? Labore
+              aliquid aliquam veritatis sunt, ex omnis rem minus voluptatem
+              placeat aut a, laborum nostrum saepe impedit, dolores enim
+              provident! Inventore, odit. Nisi ipsa dignissimos delectus a alias
+              nobis illum ullam ea. Quo veniam id maiores earum nam modi quis
+              nulla ipsam doloribus culpa odit, fugit eveniet inventore magni
+              velit voluptatibus ad cumque tempora quas consequatur, placeat non
+              excepturi. Quis similique iste minima optio, doloremque dolore eos
+              excepturi, itaque esse error sapiente, id repellendus. Odit ipsum
+              consectetur qui commodi voluptates at nam fugiat officiis
+              voluptatem fugit adipisci facilis aut quia maiores quod, placeat
+              ex quae non doloremque inventore amet voluptas? Optio, sed
+              repellendus earum mollitia eaque, amet eveniet illo voluptates est
+              ex assumenda ipsam aliquid voluptate quasi quas animi magnam
+              iusto, accusantium eum repudiandae rerum tenetur corporis cumque.
+              Ad praesentium eveniet voluptatibus, laboriosam quis porro facere
+              nostrum nobis illo? Recusandae commodi officia in temporibus vitae
+              blanditiis nostrum autem quidem sed iste beatae soluta iure rerum
+              a debitis quia laboriosam omnis aut perspiciatis, illo neque sint
+              perferendis qui! Necessitatibus, officiis? Ducimus, officiis cum.
+              Ipsam impedit, eaque non saepe doloremque fugiat eius suscipit,
+              quibusdam ad maiores eos provident aliquam. Totam, officia!
+              Eveniet quas, fugiat aspernatur iste iure recusandae molestiae a
+              ad optio esse soluta? Natus, quae. Laboriosam inventore odit
+              dolorem?
+            </p>
+          </div>
+        </div>
+        <div className='snipDetBottom'>
+          <h1>Example Explained</h1>
+          <div className='snipExampleExplained'>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Repellendus dolor ut quam asperiores aspernatur temporibus
+              architecto? Quas sit quibusdam minima nam, recusandae culpa
+              tempora nesciunt, incidunt doloribus commodi cumque ad nobis eaque
+              suscipit sequi error omnis. Quod, corporis accusantium ex
+              obcaecati quam vero repudiandae velit cupiditate facere, similique
+              porro explicabo quisquam. Eos repudiandae sit voluptatem aliquid
+              alias nostrum veniam odio ab voluptates, nulla rem accusamus illum
+              corporis aliquam dignissimos nihil delectus. Nisi cumque
+              recusandae similique, expedita maxime qui eligendi doloribus
+              quibusdam quisquam obcaecati alias adipisci rem blanditiis illum
+              soluta distinctio ab commodi dolorem consequatur mollitia porro
+              reprehenderit quia ea labore. Totam amet cupiditate nam
+              exercitationem sunt dolorum fuga necessitatibus dolore. Eos atque
+              laudantium suscipit a reprehenderit id veniam quo impedit
+              accusantium dolore vel consectetur et ipsam asperiores eaque,
+              voluptatum adipisci quis error! Consectetur tenetur molestiae
+              assumenda, autem eum rerum pariatur maxime eius sit animi deserunt
+              voluptate ex nam blanditiis laborum corrupti ducimus! Aliquid
+              earum amet eius ratione inventore omnis est cupiditate sint fuga
+              accusantium ea corrupti architecto odio iusto quae adipisci ad,
+              minus odit. Est omnis corrupti mollitia quod, animi, error ratione
+              dignissimos quaerat commodi molestias ipsa eum ex nihil
+              voluptatibus officia non dolorem unde odit obcaecati similique
+              accusamus veniam!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SnippetDetails;
