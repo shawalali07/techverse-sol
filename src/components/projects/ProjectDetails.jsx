11a029c37358638bdc7ee2485ac98942115b037e
Shawal Ali <shawalnetwork@gmail.com>
2022-12-28 21:38:33 +0500

    changes

--- src/components/projects/ProjectDetails.jsx
+++ src/components/projects/ProjectDetails.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 const ProjectDetails = () => {
-  const projects = useSelector((state) => state?.authSlice?.projects);
+  const {
+    state: { data: projects },
+  } = useLocation();
   return (
     <div style={{ paddingTop: '120px' }} className='user projectCont'>
-      {projects?.map((project) => (
-        <div className='userUpdate'>
-          <span className='userUpdateTitle'>{project?.projectTitle}</span>
-          <p className='devAboutText'>{project?.projectDescription}</p>
+      {projects?.map((project, index) => (
+        <div className='projDetDevCard'>
+          <h1>Project # {index + 1}</h1>
+          <span className=''>{project?.projectTitle}</span>
+          <p className=''>{project?.projectDescription}</p>
           <div style={{ fontWeight: 'bold' }}>Website</div>
 
-          <div className='pb-3'>
-            <i className=''>{project?.projectLink}</i>
+          <div className=''>
+            <a href={project?.projectLink} className=''>
+              {project?.projectLink}
+            </a>
           </div>
         </div>
       ))}
