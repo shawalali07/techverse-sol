5bebffd532ae7781a1c39f65f8192c7c8e7e992f
Shawal Ali <shawalnetwork@gmail.com>
2022-12-28 22:17:00 +0500

    changes

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -37,19 +37,48 @@ const Header = () => {
     <>
       <nav className={`navbar navbar-expand-lg navbar-dark topBar`}>
         <div className='w-100 px-5'>
-          <button
-            className='navbar-toggler'
-            type='button'
-            data-bs-toggle='collapse'
-            data-bs-target='#navbarSupportedContent'
-            aria-controls='navbarSupportedContent'
-            aria-expanded='false'
-            aria-label='Toggle navigation'
-            onClick={() => setShow(!show)}
-          >
-            <span className='navbar-toggler-icon'></span>
-          </button>
-          {show && <div>Here will be something</div>}
+          <div className='d-block d-lg-none'>
+            <button
+              class='btn btn-primary '
+              type='button'
+              data-bs-toggle='offcanvas'
+              data-bs-target='#offcanvasWithBothOptions'
+              aria-controls='offcanvasWithBothOptions'
+              onClick={() => setShow(!show)}
+            >
+              <span className='navbar-toggler-icon'></span>
+            </button>
+            {show && (
+              <div
+                class='offcanvas offcanvas-start'
+                data-bs-scroll='true'
+                tabindex='-1'
+                id='offcanvasWithBothOptions'
+                aria-labelledby='offcanvasWithBothOptionsLabel'
+              >
+                <div class='offcanvas-header'>
+                  <h5
+                    class='offcanvas-title'
+                    id='offcanvasWithBothOptionsLabel'
+                  >
+                    Backdrop with scrolling
+                  </h5>
+                  <button
+                    type='button'
+                    class='btn-close'
+                    data-bs-dismiss='offcanvas'
+                    aria-label='Close'
+                  ></button>
+                </div>
+                <div class='offcanvas-body'>
+                  <p>
+                    Try scrolling the rest of the page to see this option in
+                    action.
+                  </p>
+                </div>
+              </div>
+            )}
+          </div>
           <div
             className='collapse navbar-collapse d-none d-lg-flex justify-content-between w-100'
             id='navbarSupportedContent'
